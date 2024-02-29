a34fdf52f474abe99e32ae3fcdebe9c9dafbbe49
ABDOULAKADRI Boureima <diallokader9@gmail.com>
2024-03-01 00:25:41 +0100

    exportAction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Calculator from './Calculator'; 
-
+import Calculator from './Calculator';
+import ExportCSV from './ExportCSV';
 
 function App() {
   return (
     <div className="App">
+      <h1>Notation Polonaise Inverse Calculatrice</h1>
       <Calculator />
+      <ExportCSV />
     </div>
   );
 }
